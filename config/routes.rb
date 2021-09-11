e868a3ac2af1117b5fcf8ac4a97a04c9406a0005
Lubellum <azukyun0202@gmail.com>
2021-09-11 18:13:00 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+<<<<<<< HEAD
   root to: 'posts#index'
+=======
+>>>>>>> parent of 0ba78d8 ('posts#index'へのルーティング設定)
 end
